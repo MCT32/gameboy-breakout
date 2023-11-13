d7b11bb7c033de2d054157c86c74b9f2182fa613
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-11-13 17:06:13 +1100

    tile data loading (still needs more initialization)

--- main.asm
+++ main.asm
@@ -4,4 +4,37 @@ SECTION "Header", ROM0[$100]
     ds $150 - @, 0 ; room for header
 
 EntryPoint:
+    ; disable audio
+    ld a, 0
+    ld [$ff26], a ; NR52
+
+    ; wait for vblank
+    ld hl, $ff41 ; STAT register
+.wait:
+    bit 1, [hl]
+    jr nz, .wait
+
+    ; disable display
+    ld a, 0
+    ld [$ff40], a ; LCD control
+
+    ; load tile data
+    ld de, Tiles
+    ld hl, $9000 ; tile data start for signed addressing
+    ld bc, TilesEnd - Tiles
+.copyTiles:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, .copyTiles
+
     jp @
+
+SECTION "Tile data", ROM0
+
+Tiles:
+INCBIN "tiles.bin"
+TilesEnd:
